37e30697872ff58e5d503a6b13eb7748932b1140
Kate-indigo21 <kate.bolanos@indigo21.com>
2025-03-31 10:41:05 +0100

    v5

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
             class="flex xl:flex-row xs:flex-col justify-center xl:px-[240px] xs:px-[15px] xl:mb-[140px] xs:mb-[58px] xl:gap-[40px] xs:gap-[10px]">
             <!-- NEWS SHAREPOINT LIST -->
             <div class="content-container" id="news-container" data-sharepoint-list="News" data-list-count="3"
-                data-limit="3" data-sort="Created:desc">
+                data-limit="4" data-sort="Created:desc">
                 <script type="text/template" class="content-template">
                                 <!-- Place your HTML code here -->
                 <div class="news-content">
